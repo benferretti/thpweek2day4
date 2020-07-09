8dbaf8ab8c2e6dc34ce71482526b5e872932bc9d
Benoit FERRETTI <56157043+benferretti@users.noreply.github.com>
2020-07-09 14:57:58 +0200

    version finale

---
+++ 00_journalists.rb
@@ -0,0 +1,200 @@
+def initialisation
+	liste = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+	liste.each do |i|
+		i.delete! '@'  #enlève les @
+	end
+	return liste
+end
+	
+def question1 (liste)
+	nombre_handle = 0
+	liste.each do |i|
+		nombre_handle += 1
+	end
+	puts "Il y a #{nombre_handle} handles"
+end
+
+def question2 (liste)
+	count_char = 0
+	count_char_min = 0
+	handle_court = ""
+	array = {}
+	liste.each do |i|
+		chars = i.split('')  #découpe les tags en lettres
+		chars.each do |c| #boucle des lettres des tags du string
+	    	count_char += 1
+	    end
+	    array[i] = count_char
+	    #condition handle le plus court
+		if (count_char_min > count_char || count_char_min == 0)
+			handle_court = "#{i}"
+			count_char_min = count_char
+		end
+		count_char = 0
+	end
+	puts "Le handle le plus court est @#{handle_court}, il a #{count_char_min} caractères"	
+end
+
+def question3 (liste)
+	count_char = 0
+	count_cinq_char = 0
+	array = {}
+	tableau_cinq_char = []
+	liste.each do |i|
+		chars = i.split('')  #découpe les tags en lettres
+		chars.each do |c| #boucle des lettres des tags du string
+	    	count_char += 1
+	    end
+	    array[i] = count_char
+	    #condition handle le plus court
+		if (count_char == 5)
+			tableau_cinq_char << "#{i}"
+			count_cinq_char += 1
+		end
+		count_char = 0
+	end
+	puts "Il y a #{count_cinq_char} handles à 5 caractères : #{tableau_cinq_char}"
+end
+	
+def question4 (liste)
+	tableau_majuscule = []
+	count_majuscule = 0
+
+	liste.each do |i|
+		chars = i.split('')  #découpe les tags en lettres
+		if (chars[0] == chars[0].upcase)
+			tableau_majuscule << i
+			count_majuscule += 1
+		end
+	end
+	puts "Il y a #{count_majuscule} handles qui commencent par une majuscule"
+end
+
+def question5 (liste)
+		liste_ordre_alphabetique = liste.sort_by(&:downcase)
+		liste_ordre_alphabetique.each do |i|
+			i.delete! '@'
+			puts "#{i}"  #affiche les tags sans les @
+		end
+end
+
+def question6 (liste)
+		liste_ordre_longueur = liste.sort_by(&:length)
+		liste_ordre_longueur.each do |i|
+			i.delete! '@'  #enlève les @
+		    puts "#{i}"
+		end
+end
+
+def question7 (liste)
+	count_position = 1
+	count_epenser = 0
+	liste.each do |i|
+		if i != "epenser" 
+			count_position += 1
+			#puts "#{count_position}"
+		else
+			count_epenser = count_position
+		end
+	end
+	puts "Le handle epenser est en position numéro #{count_epenser}"
+end
+
+def question8 (liste) 
+		array = {}
+		count_char = 0
+		erreur = 0
+
+		liste.each do |i|
+
+			i.delete! '@'  #enlève les @
+		  	chars = i.split('')  #découpe les tags en lettres
+
+			chars.each do |c| #boucle des lettres des tags du string
+		    	count_char += 1
+			end
+
+			array[i] = count_char
+			count_char = 0
+		end
+		puts "Les handles contenant combien de caractères voulez-vous ?"
+		
+		nbchars = gets.chomp.to_i
+		while nbchars < 3 || nbchars > 32
+			puts "Les handles contenant combien de caractères voulez-vous ?"
+			nbchars = gets.chomp.to_i
+		end
+		puts "Voici les handles avec #{nbchars} caractères :"
+		array.each do |handle, length|
+			if length == nbchars
+				puts "#{handle}"
+				erreur += 1
+			end
+		end
+		if (erreur == 0) 
+			puts "Il n'y a aucun handle pour ce nombre de caractères"
+		end
+end
+
+def front
+	go = "Y"
+	while (go == "Y" || go == "y" )
+		
+		puts ""
+		puts 'Bienvenue dans ce programme, que souhaitez-vous faire ?'
+		puts 'Tapez "1" pour la question 1, "2" pour la question 2 etc... '
+		puts ""
+		puts 'Question 1 : Combien y a-t-il de handle dans cette array ? '
+		puts 'Question 2 : Quelle est le handle le plus court de cette liste ? '
+		puts 'Question 3 : Combien y-a-t-il de handle contenant 5 caractères ?'
+		puts 'Question 4 : Combien commencent par une majuscule (première lettre juste après le @) ?'
+		puts 'Question 5 : Trie la liste de handle par ordre alphabétique.'
+		puts 'Question 6 : Trie la liste de handle par taille des handle (ordre croissant)'
+		puts "Question 7 : Quelle est la position dans l'array de la personne @epenser ?"
+		puts 'Question 8 : Sors-moi une répartition des handle par taille de ces derniers'
+		puts ""
+
+		mychoice = gets.chomp.to_i
+
+		while ( mychoice < 1 || mychoice > 8 )
+			puts "Choisissez un chiffre entre 1 et 8"
+			mychoice = gets.chomp.to_i
+		end
+
+		liste = initialisation
+
+		if mychoice == 1
+			question1 (liste)
+
+		elsif mychoice == 2
+			question2 (liste)
+
+		elsif mychoice == 3
+			question3 (liste)
+		
+		elsif mychoice == 4
+			question4 (liste)	
+		
+		elsif mychoice == 5
+			question5 (liste)
+
+		elsif mychoice == 6
+			question6 (liste)
+
+		elsif mychoice == 7	
+			question7 (liste)
+		
+		elsif mychoice == 8
+			question8 (liste)
+		end
+
+		puts ""
+		puts ("Voulez-vous continuer ? (Y/N)")
+		go = gets.chomp
+	end
+	puts ""
+	puts ("Merci et à bientôt")
+	exit
+end
+
+front
